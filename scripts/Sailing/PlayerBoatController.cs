6347905b07f436cdb02c6169648c6dfa91b94331
scotthops <scott@benjipays.com>
2026-04-23 15:33:52 -0700

    stable sailing sandbox precombat

--- scripts/Sailing/PlayerBoatController.cs
+++ scripts/Sailing/PlayerBoatController.cs
@@ -5,6 +5,8 @@ namespace TidesOfTime.Sailing;
 
 public partial class PlayerBoatController : Node3D
 {
+	private const float Tau = MathF.PI * 2.0f;
+
 	[ExportGroup("Speed")]
 	[Export] public float MaxForwardSpeed { get; set; } = 14.0f;
 	[Export] public float MaxReverseSpeed { get; set; } = 3.2f;
@@ -25,13 +27,25 @@ public partial class PlayerBoatController : Node3D
 	[Export] public float LeanResponsiveness { get; set; } = 7.0f;
 	[Export] public float PitchDegrees { get; set; } = 3.0f;
 	[Export] public float PitchResponsiveness { get; set; } = 4.5f;
-	[Export] public float BobHeight { get; set; } = 0.11f;
-	[Export] public float BobFrequency { get; set; } = 1.45f;
-	[Export] public float SpeedBobInfluence { get; set; } = 1.1f;
+
+	[ExportGroup("Fake Waves")]
+	[Export] public NodePath VisualRootPath { get; set; } = new("Visuals");
+	[Export] public float WaveHeight { get; set; } = 0.18f;
+	[Export] public float WaveLength { get; set; } = 14.0f;
+	[Export] public float WaveFrequency { get; set; } = 0.16f;
+	[Export] public float SecondaryWaveHeight { get; set; } = 0.06f;
+	[Export] public float SecondaryWaveLength { get; set; } = 7.5f;
+	[Export] public float SecondaryWaveFrequency { get; set; } = 0.27f;
+	[Export] public float WaveSampleDistance { get; set; } = 2.0f;
+	[Export] public float WaveSmoothing { get; set; } = 7.5f;
+	[Export] public float WavePitchDegrees { get; set; } = 4.5f;
+	[Export] public float WaveRollDegrees { get; set; } = 5.5f;
 
 	public Vector3 Velocity => _velocity;
 	public float Speed => new Vector2(_velocity.X, _velocity.Z).Length();
 
+	private Node3D? _visualRoot;
+	private Vector3 _visualRestPosition;
 	private Vector3 _velocity = Vector3.Zero;
 	private Vector3 _startPosition;
 	private float _startYaw;
@@ -39,12 +53,26 @@ public partial class PlayerBoatController : Node3D
 	private float _yaw;
 	private float _pitch;
 	private float _roll;
-	private float _bobTime;
 	private float _smoothedTurnInput;
+	private float _waveTime;
+	private float _waveBob;
+	private float _wavePitch;
+	private float _waveRoll;
 
 	public override void _Ready()
 	{
 		ProcessPriority = -20;
+		_visualRoot = GetNodeOrNull<Node3D>(VisualRootPath);
+
+		if (_visualRoot == null)
+		{
+			GD.PushWarning("PlayerBoatController: VisualRootPath is not assigned.");
+		}
+		else
+		{
+			_visualRestPosition = _visualRoot.Position;
+		}
+
 		_startPosition = GlobalPosition;
 		_startYaw = Rotation.Y;
 		_waterHeight = GlobalPosition.Y;
@@ -70,8 +98,11 @@ public partial class PlayerBoatController : Node3D
 		ApplyTurning(turnInput, throttleInput, forwardSpeed, deltaSeconds);
 
 		var nextPosition = GlobalPosition + (_velocity * deltaSeconds);
-		nextPosition.Y = _waterHeight + GetBobOffset(deltaSeconds);
+		nextPosition.Y = _waterHeight;
 		GlobalPosition = nextPosition;
+
+		_waveTime += deltaSeconds;
+		UpdateWavePresentation(deltaSeconds);
 	}
 
 	public void ResetToStart()
@@ -80,10 +111,19 @@ public partial class PlayerBoatController : Node3D
 		_yaw = _startYaw;
 		_pitch = 0.0f;
 		_roll = 0.0f;
-		_bobTime = 0.0f;
 		_smoothedTurnInput = 0.0f;
+		_waveTime = 0.0f;
+		_waveBob = 0.0f;
+		_wavePitch = 0.0f;
+		_waveRoll = 0.0f;
 		GlobalPosition = _startPosition;
 		Rotation = new Vector3(0.0f, _yaw, 0.0f);
+
+		if (_visualRoot != null)
+		{
+			_visualRoot.Position = _visualRestPosition;
+			_visualRoot.Rotation = Vector3.Zero;
+		}
 	}
 
 	private void ApplyTurning(float turnInput, float throttleInput, float forwardSpeed, float deltaSeconds)
@@ -103,7 +143,7 @@ public partial class PlayerBoatController : Node3D
 		var desiredRoll = DegreesToRadians(-_smoothedTurnInput * LeanDegrees) * speedRatio;
 		_pitch = Mathf.Lerp(_pitch, desiredPitch, GetSmoothingWeight(PitchResponsiveness, deltaSeconds));
 		_roll = Mathf.Lerp(_roll, desiredRoll, GetSmoothingWeight(LeanResponsiveness, deltaSeconds));
-		Rotation = new Vector3(_pitch, _yaw, _roll);
+		Rotation = new Vector3(0.0f, _yaw, 0.0f);
 	}
 
 	private float ApplyPlanarDrag(Vector3 forward, float throttleInput, bool hardBrakeHeld, float deltaSeconds)
@@ -127,15 +167,44 @@ public partial class PlayerBoatController : Node3D
 		return forwardSpeed;
 	}
 
-	private float GetBobOffset(float deltaSeconds)
+	private void UpdateWavePresentation(float deltaSeconds)
 	{
-		var speedRatio = Mathf.Clamp(Speed / Mathf.Max(0.01f, MaxForwardSpeed), 0.0f, 1.0f);
-		_bobTime += deltaSeconds * (BobFrequency + (speedRatio * SpeedBobInfluence));
+		if (_visualRoot == null)
+		{
+			return;
+		}
 
-		var primaryBob = MathF.Sin(_bobTime);
-		var secondaryBob = MathF.Sin((_bobTime * 1.73f) + 0.7f) * 0.35f;
+		var sampleDistance = Mathf.Max(0.01f, WaveSampleDistance);
+		var forward = GetFlatForward();
+		var right = GetFlatRight();
+		var center = GlobalPosition;
+		var centerHeight = GetWaveHeightAt(center);
+		var frontHeight = GetWaveHeightAt(center + (forward * sampleDistance));
+		var backHeight = GetWaveHeightAt(center - (forward * sampleDistance));
+		var rightHeight = GetWaveHeightAt(center + (right * sampleDistance));
+		var leftHeight = GetWaveHeightAt(center - (right * sampleDistance));
+		var heightRange = sampleDistance * 2.0f;
+		var targetWavePitch = Mathf.Clamp((backHeight - frontHeight) / heightRange, -1.0f, 1.0f) * DegreesToRadians(WavePitchDegrees);
+		var targetWaveRoll = Mathf.Clamp((rightHeight - leftHeight) / heightRange, -1.0f, 1.0f) * DegreesToRadians(WaveRollDegrees);
+		var smoothingWeight = GetSmoothingWeight(WaveSmoothing, deltaSeconds);
 
-		return (primaryBob + secondaryBob) * BobHeight * (0.3f + (speedRatio * 0.7f));
+		_waveBob = Mathf.Lerp(_waveBob, centerHeight, smoothingWeight);
+		_wavePitch = Mathf.Lerp(_wavePitch, targetWavePitch, smoothingWeight);
+		_waveRoll = Mathf.Lerp(_waveRoll, targetWaveRoll, smoothingWeight);
+
+		_visualRoot.Position = _visualRestPosition + (Vector3.Up * _waveBob);
+		_visualRoot.Rotation = new Vector3(_pitch + _wavePitch, 0.0f, _roll + _waveRoll);
+	}
+
+	private float GetWaveHeightAt(Vector3 worldPosition)
+	{
+		var primaryPhase = (((worldPosition.X * 0.35f) + (worldPosition.Z * 0.94f)) / Mathf.Max(0.01f, WaveLength) * Tau)
+			+ (_waveTime * WaveFrequency * Tau);
+		var secondaryPhase = (((worldPosition.X * -0.8f) + (worldPosition.Z * 0.45f)) / Mathf.Max(0.01f, SecondaryWaveLength) * Tau)
+			+ (_waveTime * SecondaryWaveFrequency * Tau)
+			+ 1.4f;
+
+		return (MathF.Sin(primaryPhase) * WaveHeight) + (MathF.Sin(secondaryPhase) * SecondaryWaveHeight);
 	}
 
 	private Vector3 GetFlatForward()
@@ -144,6 +213,14 @@ public partial class PlayerBoatController : Node3D
 		forward.Y = 0.0f;
 
 		return forward.LengthSquared() <= 0.0001f ? Vector3.Forward : forward.Normalized();
+	}
+
+	private Vector3 GetFlatRight()
+	{
+		var right = GlobalTransform.Basis.X;
+		right.Y = 0.0f;
+
+		return right.LengthSquared() <= 0.0001f ? Vector3.Right : right.Normalized();
 	}
 
 	private static float GetThrottleInput()
